ffd7ffd845ef85955c69f12173c6a6612b6847bf
Paulina Reus <paularkla@gmail.com>
2023-03-27 12:14:37 +0200

    Added rectangle

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -7,3 +7,13 @@ pole = pow(a, 2)
 print("Obwód kwardratu wynosi", obwod, "a pole", pole)
 print("Obwód kwardratu wynosi {} a pole {}".format(obwod, pole))
 
+#Prostokąt
+
+b = 10
+c = 20
+
+obwod_p = 2*b + 2* c
+pole_p = b * c
+
+print("Obwód prostokąta wynosi {} a pole {}".format(obwod_p, pole_p))
+
